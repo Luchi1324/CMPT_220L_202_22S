44d97494b1ef5f41437fb63cc3bab011c8c021da
Luchi1324 <luchileigh8@gmail.com>
2022-02-23 11:46:06 -0500

    Assignment 4 Submission

--- hw/4/Problem1.java
+++ hw/4/Problem1.java
@@ -1,6 +1,16 @@
+import java.util.*;
 public class Problem1 {
     public static void main(String[] args){
-        // Testing git and vscode integration
-        System.out.println("github is dope");
+        // Gets input
+        try (Scanner input = new Scanner(System.in)) {
+            System.out.print("Enter the number of sides: ");
+            int n = input.nextInt(); 
+            System.out.print("Enter the side: ");
+            double s = input.nextDouble();
+
+            // Computes then outputs results
+            double area = (n * (s*s)) / (4 * Math.tan(Math.PI/n));
+            System.out.println("The area of the polygon is "+area);
+        }
     }
 }
