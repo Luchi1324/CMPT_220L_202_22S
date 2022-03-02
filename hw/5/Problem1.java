4d0462bedb3d5e19d8264b2f4f13a85cb514ec1a
Luchi1324 <luchileigh8@gmail.com>
2022-03-02 16:47:00 -0500

    Merge branch 'main' of https://github.com/Luchi1324/cmpt220mattoli

--- hw/5/Problem1.java
+++ hw/5/Problem1.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
 public class Problem1 {
     public static void main(String[] args) {
-
+        // Gets user input
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the integer to multiply it's digits: ");
+        long nInput = input.nextLong();
+        input.close();
+        // Calls function, prints result
+        System.out.println(multiplyDigits(nInput));
+    }
+    public static int multiplyDigits(long n) {
+        int product = 1;
+        // Until n is fully divided, extracts digits by % 10
+        while (n != 0) {
+            product = (int) (product * (n % 10));
+            n = n / 10;
+        }
+        // Returns computed result
+        return product;
     }
 }
