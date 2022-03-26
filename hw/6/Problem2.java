4e8211cfbe432ff58d1d0034a71c7cc793072395
Luchi1324 <luchileigh8@gmail.com>
2022-03-25 23:53:57 -0400

    Assignment 6 Submission

---
+++ hw/6/Problem2.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+class Point {
+    double x;
+    double y;
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+    public String toString() {
+        return ("(" + x + "," + y + ")"); 
+    }
+}
+
+public class Problem2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        Point[] points = new Point[6];
+
+        for (int i = 0; i < points.length; i++) {
+            System.out.print("Enter x"+(i+1)+", y"+(i+1)+": ");
+            int x = input.nextInt();
+            int y = input.nextInt();
+            points[i] = new Point(x, y);
+        }
+        input.close();
+        for (int i = 0; i < points.length; i++) {
+            System.out.println(points[i]);
+        }
+    }
+
+    public static double cross_product(Point a, Point b) {
+        return a.x * b.y - a.y * b.x;
+    }
+
+    public static double area(Point[] vertices) {
+        double sum = 0.0
+        for (int i = 0; i < 5; i++) {
+            sum += cross_product(
+                vertices[i],
+                vertices[i+1]%5);
+        }
+    }
+}
---
+++ hw/6/Problem3.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Problem3 {
+    public static int[] count(String s) {
+        int[] counts = new int[10];
+        for (int i = 0; i < counts.length; i++) {
+            int count = 0;
+            for (int j = 0; i < counts.length; i++) {
+                if (s.charAt(i) == s.charAt(j)) {
+                    count++;
+                }
+            counts[i] = count;
+            }
+        }
+        return counts;
+    }
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String str = input.nextLine();
+        input.close();
+        count(str);
+    }
+}
